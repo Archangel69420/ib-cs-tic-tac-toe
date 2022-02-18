c7145311a9b924d480f6af6d28d298c45f3fab46
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2022-02-18 11:37:58 +0200

    HW

--- TicTacToe.java
+++ TicTacToe.java
@@ -22,8 +22,38 @@ public class TicTacToe
 
   boolean gameOver ()
   {
-    // code to be written
-    return false; // this line must also be removed
+    int counter1 = 0, counter2 = 0, counter3 = 0, counter4 = 0, counter5 = 0, counter6 = 0, counter7 = 0, counter8 = 0;
+    boolean flag = false;
+    while (!flag) {
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+          if (marks[i][j] == Mark.Cross)
+            counter1++;
+          if (marks[i][j] == Mark.Nought)
+            counter2++;
+          if (marks[j][i] == Mark.Cross)
+            counter3++;
+          if (marks[j][i] == Mark.Nought)
+            counter4++;
+        }
+        if (counter1 ==N || counter2 == N || counter3 == N || counter4 == N)
+          flag = true;
+        else {
+          counter1 = 0; counter2 = 0; counter3 = 0; counter4 = 0;
+        }
+        if (marks[i][i] == Mark.Cross)
+          counter5++;
+        if (marks[i][i] == Mark.Nought)
+          counter6++;
+        if (marks[N - 1- i][i] == Mark.Cross)
+          counter7++;
+        if (marks[N - 1 - i][i] == Mark.Nought)
+          counter8++;
+      }
+      if (counter5 == N || counter6 == N || counter7 == N || counter8 == N)
+        flag = true;
+    }
+    return flag;
   }
   
   private final int N = 3;
